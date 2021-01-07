6ac49d2364447613f798ce09ccaf3b0a1d3ef649
Simone <simone.bresci.05@gmail.com>
2021-01-07 16:08:05 +0100

    ESERCIZIO COMPLETATO

--- js/script.js
+++ js/script.js
@@ -1,28 +1,120 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
-// 
-// var inputNum = 0;
-// var somma = 0;
-// var arrayRandom = [ ];
-// var lunghezzaArray = 4;
-//
-// // Acquisisci elemento arrayRandom
-// for (var x = 0; x < lunghezzaArray; x++) {
-//   arrayRandom.push(parseInt(Math.random() * 10 ));
-//   console.log(arrayRandom[x]);
-// }
-//
-// console.log("Array lungo: " + arrayRandom.length);
-//
-// // Somma dispari
-// for (var i = 1; i <= arrayRandom.length; i++) {
-//   if ( (i % 2) !== 0){
-//     console.log('elemento' + parseInt(arrayRandom[i-1]));
-//     somma += parseInt(arrayRandom[i-1]);
-//     console.log('somma' + somma);
-//   }
-//
-//   // console.log('elemento' + i);
-// }
-//
-// // Stampa a video
-// console.log('FOR: La somma vale' + somma);
+// Controlla se parola in input è palindroma
+function isPalindroma(parola){
+  var result = true; // true se parola palindroma
+
+  // Controlla lunghezza parola
+  if (parola.length < 1 ){
+    result = false;
+  }
+
+
+  // Controllo incrociato tra inizio e fine parola
+  // Termina controllo a metà parola + 1
+  for (var i = 0; i < (parola.length / 2) + 1 && (result === true); i++) {
+    if(parola.charAt(i) !== parola.charAt(parola.length - 1 - i)){
+      result = false;
+    }
+  }
+
+  return result;
+}
+
+// Controlla se numero in input è compreso fra min e max
+function checkMinMax(num, min, max) {
+  var result = true; // true == numero compreso fra min e max
+  num = parseInt(num);
+  if(num < min || num > max){
+    result = false;
+  }
+  return result;
+}
+
+// Funzione random custom
+function randomCustom(min, max){
+  return parseInt(Math.random() * (max-min) + min);
+}
+
+// Controlla se numero è pari o meno
+function isPari(num){
+  var result;
+  num = parseInt(num);
+  if (num % 2 === 0 ){
+    result = true;
+  }else {
+    result = false;
+  }
+
+  return result;
+}
+
+
+// Palindroma
+var input = prompt('Inserisci parola');
+var esito = false;
+
+esito = isPalindroma(input);
+if (esito){
+  console.log('La parola inserita è palindroma: ' + input);
+}else{
+  console.log('La parola inserita non è palindroma: ' + input);
+}
+
+
+// GIOCO PARI E DISPARI
+var utentePari = false;
+var input = '';
+var numUtente = 0;
+var numCPU = 0;
+var numFinale = 0;
+
+// Scegli pari o dispari
+do{
+  input = prompt('Seleziona pari (p) oppure dispari (d)');
+  if (input ==='p'){
+    utentePari = true;
+    console.log('Utente: pari');
+    console.log('CPU   : dispari');
+  }
+
+  if(input ==='d'){
+    utentePari = false;
+    console.log('Utente: dispari');
+    console.log('CPU   : pari');
+  }
+}while( input !== 'p' && input !== 'd' )
+
+
+// Scegli un numero da 1 a 5
+do{
+  numUtente = prompt('Scegli numero da 1 a 5');
+}while(!checkMinMax(numUtente, 1, 5));
+
+console.log('Utente: ' + numUtente);
+
+// Genera numero casuale per l'avversario
+numCPU = randomCustom(1, 5);
+console.log('CPU   : ' + numCPU);
+
+// Stabilisci vincitore
+console.log("The winner is...");
+
+// Somma numeri giocatori
+numFinale = parseInt(numUtente) + parseInt(numCPU);
+console.log('NumFinale: ' + numFinale);
+
+// Stabilisci vincitore
+if (isPari(numFinale) && utentePari){
+  console.log('Utente vince');
+}else{
+  console.log('CPU vince');
+}
