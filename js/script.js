ebbdd84062b44f2143fa14b8e459fda5a9906096
Simone <simone.bresci.05@gmail.com>
2021-01-07 14:54:45 +0100

    inizio progetto

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// 
+// var inputNum = 0;
+// var somma = 0;
+// var arrayRandom = [ ];
+// var lunghezzaArray = 4;
+//
+// // Acquisisci elemento arrayRandom
+// for (var x = 0; x < lunghezzaArray; x++) {
+//   arrayRandom.push(parseInt(Math.random() * 10 ));
+//   console.log(arrayRandom[x]);
+// }
+//
+// console.log("Array lungo: " + arrayRandom.length);
+//
+// // Somma dispari
+// for (var i = 1; i <= arrayRandom.length; i++) {
+//   if ( (i % 2) !== 0){
+//     console.log('elemento' + parseInt(arrayRandom[i-1]));
+//     somma += parseInt(arrayRandom[i-1]);
+//     console.log('somma' + somma);
+//   }
+//
+//   // console.log('elemento' + i);
+// }
+//
+// // Stampa a video
+// console.log('FOR: La somma vale' + somma);
